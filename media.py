360328cd57a758d3c4f748a1cece85273aed2ecb
Rob <robmessenger@iMac.lan>
2018-03-21 12:53:08 +0100

    fix error in showTrailer func

--- media.py
+++ media.py
@@ -1,13 +1,11 @@
 import webbrowser
 
-class Movie(): #capitalisation per google sytle guide
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_url): #__init__ is a built-in function. self refers to the instance
-        #all these attributes must be provided when the class is instantiated
-        #they become instance variables
+class Movie():
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_url):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_url
 
     def showTrailer(self):
-        webbrowser.open(self.trailer)
+        webbrowser.open(self.trailer_youtube_url)
