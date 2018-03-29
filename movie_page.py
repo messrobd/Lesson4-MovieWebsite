bfd043a95a7044476c0a0519ec90f03c098cf8da
Rob <robmessenger@iMac.lan>
2018-03-29 14:30:03 +0200

    implement test_modal_player changes in production system

--- movie_page.py
+++ movie_page.py
@@ -14,22 +14,62 @@ page_layout = """
         <h1>Rob's movies</h1>
     </div>
     <div class="movie-list">{movie_tiles}</div>
+    <div id="modal">
+      <div id="player-container">
+        <div>
+          <span id="dismissPlayerControl">&times;</a>
+        </div>
+        <div>
+          <iframe id="player" width="560" height="315" src="" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+      </div>
+    </div>
+    <script>{script}</script>
   </body>
 </html>
 """
 
 movie_tile = """
 <div class="movie-tile">
-    <div class="image-container">
-        <a href="{trailer_youtube_url}" target="_blank">
-            <img class="movie-image" src="{poster_image_url}">
-        </a>
+    <div id="{movie_title}" class="image-container">
+        <img class="movie-image" src="{poster_image_url}">
     </div>
     <div class="attribution">
         <p class="attribution-text">{poster_image_attribution}</p>
     </div>
     <h2 class="title-container">{movie_title}</h2>
 </div>
+"""
+
+script = """
+      var modalElement = document.getElementById('modal');
+      var playerElement = document.getElementById('player');
+
+      function invokePlayer(trailerURL){{
+        playerElement.src = trailerURL
+        modalElement.style.display = 'flex'
+      }}
+      function dismissPlayer(){{
+        modalElement.style.display = 'none'
+        playerElement.src = ''
+      }}
+
+      function makeInvokeHandler(trailerURL) {{
+        return function () {{
+          invokePlayer(trailerURL);
+        }}
+      }}
+
+      var trailerDict = {trailerDict};
+
+      for (var movie in trailerDict) {{
+        var movieTile = document.getElementById(movie);
+        var trailerURL = trailerDict[movie];
+        movieTile.onclick = makeInvokeHandler(trailerURL);
+      }}
+
+      var dismissPlayerControl = document.getElementById('dismissPlayerControl');
+      dismissPlayerControl.onclick = dismissPlayer;
 """
 
 def makeTiles(movies):
@@ -45,13 +85,22 @@ def makeTiles(movies):
     movie_tiles = ""
     for movie in movies:
         movie_tiles += movie_tile.format(
-        trailer_youtube_url = movie.trailer_youtube_url,
+        movie_title = movie.title,
         poster_image_url = movie.poster_image_url,
         poster_image_attribution = movie.poster_image_attribution,
-        movie_title = movie.title
         )
 
     return movie_tiles
+
+def makeScript(movies):
+    trailerDict = {}
+    for movie in movies:
+        movieID = movie.title
+        trailerURL = movie.trailer_youtube_url
+        trailerDict[movieID] = trailerURL
+
+    return script.format(trailerDict = str(trailerDict))
+    #print str(trailerDict)
 
 def makePage(movies):
     """
@@ -62,7 +111,7 @@ def makePage(movies):
        ii. an html fragment as a string defining the structure of the web page,
        in which a variable stands in for the movie list html to be substituted
     3. outputs: an html file substituting movie list html elements into the
-    input html string 
+    input html string
     """
     initial_working_dir = os.getcwd()
     project_folder = os.path.dirname(__file__)
@@ -70,7 +119,7 @@ def makePage(movies):
 
     movie_webpage_file = open("movie_webpage.html", "w")
 
-    composed_page = page_layout.format(movie_tiles = makeTiles(movies))
+    composed_page = page_layout.format(movie_tiles = makeTiles(movies), script = makeScript(movies))
 
     movie_webpage_file.write(composed_page)
     movie_webpage_file.close()
